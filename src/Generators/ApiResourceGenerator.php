9e982f43a9dbc8d2ed5ed895acb44983e995a8d6
Opeyemi Aina <barmmie@gmail.com>
2017-11-05 13:06:35 +0100

    custom generators

---
+++ src/Generators/ApiResourceGenerator.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace InfyOm\Generator\Generators;
+
+use InfyOm\Generator\Common\CommandData;
+use InfyOm\Generator\Utils\FileUtil;
+
+class ApiResourceGenerator extends BaseGenerator
+{
+    /** @var CommandData */
+    private $commandData;
+
+    /** @var string */
+    private $path;
+
+    /** @var string */
+    private $fileName;
+
+    public function __construct(CommandData $commandData)
+    {
+        $this->commandData = $commandData;
+        $this->path = $commandData->config->pathResource;
+        $this->fileName = $this->commandData->modelName.'Resource.php';
+    }
+
+    public function generate()
+    {
+        $templateData = get_template('resource', 'laravel-generator');
+
+        $templateData = fill_template($this->commandData->dynamicVars, $templateData);
+
+        $docsTemplate = get_template('docs.resource', 'laravel-generator');
+        $docsTemplate = fill_template($this->commandData->dynamicVars, $docsTemplate);
+        $docsTemplate = str_replace('$GENERATE_DATE$', date('F j, Y, g:i a T'), $docsTemplate);
+
+        $templateData = str_replace('$DOCS$', $docsTemplate, $templateData);
+        $templateData = str_replace('$FIELDS$', $this->generateFields(), $templateData);
+
+        FileUtil::createFile($this->path, $this->fileName, $templateData);
+
+        $this->commandData->commandComment("\n Resource created: ");
+        $this->commandData->commandInfo($this->fileName);
+    }
+
+    public function rollback()
+    {
+        if ($this->rollbackFile($this->path, $this->fileName)) {
+            $this->commandData->commandComment('Service file deleted: '.$this->fileName);
+        }
+    }
+
+    private function generateFields()
+    {
+        $fields = [];
+
+        foreach ($this->commandData->fields as $field) {
+
+            $fields[] = "'".$field->name. "' => ". '$this->'.$field->name.',';
+
+        }
+
+        return implode(infy_nl_tab(1, 3), array_merge($fields));
+    }
+}
